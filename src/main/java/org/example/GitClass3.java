77a39de88709e57a0cf17c3c000f67cd6524eb4d
nazgul <nazgul@Nazguls-MacBook-Pro.local>
2023-07-01 15:16:37 -0400

    PR

--- src/main/java/org/example/GitClass3.java
+++ src/main/java/org/example/GitClass3.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class GitClass3 {
     public static void main(String[] args) {
-        System.out.println("Branch");
+        System.out.println("NewBranch");
+        //we are trying to see the git PR-PULL REQUEST
     }
 
 }
